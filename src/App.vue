3903e0ab47c805cef597ba0aa9ee53fe6b55262d
Fábio Gonçalves <fabio.g@inttecnologia.com.br>
2019-02-15 19:06:52 -0200

    Estudo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <h1>Componentes Dinâmicos</h1>
+    <button @click="componentSelecionado = 'Home'">Home</button>
+    <button @click="componentSelecionado = 'PostLista'">Posts</button>
+    <button @click="componentSelecionado = 'Sobre'">Sobre</button>
+    <p>{{componentSelecionado}}</p>
+
+<component :is="componentSelecionado"></component>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PostLista from "./components/PostLista.vue";
+import Sobre from "./components/Sobre.vue";
+import Home from "./components/Home.vue";
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    Home,
+    PostLista,
+    Sobre
+  },
+  data() {
+    return {
+      componentSelecionado: "Home",
+      posts: [
+        {
+          id: 1,
+          titulo: "Componentes no Vue",
+          conteudo: "Componentes são uma das peças mais importantes",
+          autor: "Zezinho"
+        },
+        {
+          id: 2,
+          titulo: "Distribuindo conteudo com Slots",
+          conteudo: "Slots podem ser usados como repositorios",
+          autor: "Fábio Gonçalves"
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.container {
+  width: 660px;
+  margin: auto;
 }
 </style>
